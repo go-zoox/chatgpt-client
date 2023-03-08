d666a3577af8919eb3ea04cc44eb8db84c520cf2
whatwewant <tobewhatwewant@outlook.com>
2023-03-08 08:56:32 +0800

    chore: change default DefaultConversationMaxAge => 30 day

--- constants.go
+++ constants.go
@@ -13,10 +13,12 @@ const DefaultMaxRequestResponseTokens = openai.MaxTokens
 const DefaultMaxResponseTokens = 1000
 
 // DefaultMaxConversations is the default max conversation cache count.
-const DefaultMaxConversations = 1000
+const DefaultMaxConversations = 10000
 
 // DefaultConversationMaxAge is timeout for each conversation.
-const DefaultConversationMaxAge = 2 * time.Hour
+//
+//	default: 30 day
+const DefaultConversationMaxAge = 30 * 24 * time.Hour
 
 // DefaultContext is the default context for instructions to ChatGPT.
 const DefaultContext = "Instructions:\nYou are {{.chatgpt_name}}, a large language model trained by OpenAI.\nCurrent date: {{.date}}"
